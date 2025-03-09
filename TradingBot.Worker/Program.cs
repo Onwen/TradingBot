02a601d9d523f527e82b690eb2afefc22fcc4245
sean <onwen@protonmail.com>
2025-03-09 15:08:45 +1100

    changes for railway deploy

--- TradingBot.Worker/Program.cs
+++ TradingBot.Worker/Program.cs
@@ -3,7 +3,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TradingBot.Worker;
 using TradingBot.UseCases;
-using TradingBot.Infrastructure.Repository.DataContext; // Add this to reference ApplicationDbContext
+using TradingBot.Infrastructure.Repository.DataContext;
+
+// Tell Railway this is a background service, not a web service
+Environment.SetEnvironmentVariable("RAILWAY_SERVICE_TYPE", "WORKER");
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -15,10 +18,19 @@ builder.Services.ConfigureWorkers();
 var host = builder.Build();
 
 // Apply migrations before running the host
-using (var scope = host.Services.CreateScope())
+try
 {
-    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.Migrate(); // This will apply any pending migrations
+    using (var scope = host.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        dbContext.Database.Migrate(); // This will apply any pending migrations
+    }
+}
+catch (Exception ex)
+{
+    // Log migration failure but don't crash the app
+    var logger = host.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred while migrating the database.");
 }
 
 host.Run();
